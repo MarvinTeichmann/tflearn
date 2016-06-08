9f026db469776161e8ba14894549559ded0d3765
Marvin Teichmann <marvin.teichmann@googlemail.com>
2016-06-08 02:35:30 -0400

    Implement weak cross entropy 2d loss.
    This loss is widely used in semantic segmentation.
    see also: https://github.com/MarvinTeichmann/tensorflow-fcn

--- tflearn/losses.py
+++ tflearn/losses.py
@@ -45,3 +45,42 @@ def L1(tensor, wd=0.001):
 
     """
     return tf.mul(tf.reduce_sum(tf.abs(tensor)), wd, name='L1-Loss')
+    
+def weak_cross_entropy_2d_loss(logits, labels, num_classes, epsilon=0.0001, head=None):
+    """Calculate the semantic segmentation using weak softmax cross entropy loss.
+
+    Given 2d image shaped logits and corresponding labels, this calculated the
+    widely used semantic segmentation loss. Using `tf.nn.softmax_cross_entropy_with_logits`
+    is currently not supported. See https://github.com/tensorflow/tensorflow/issues/2327#issuecomment-224491229
+
+    Args:
+      logits: tensor, float - [batch_size, width, height, num_classes].
+      labels: Labels tensor, int32 - [batch_size, width, height, num_classes].
+          The ground truth of your data.
+      head: numpy array - [num_classes].
+          Weighting the loss of each class.
+
+    Returns:
+      loss: Loss tensor of type float.
+    """
+    with tf.name_scope('loss'):
+        logits = tf.reshape(logits, (-1, num_classes))
+        shape = [logits.get_shape()[0], num_classes]
+        epsilon = tf.constant(value=epsilon, shape=shape)
+        logits = logits + epsilon
+        labels = tf.to_float(tf.reshape(labels, (-1, num_classes)))
+
+        softmax = tf.nn.softmax(logits)
+
+        if head is not None:
+            cross_entropy = -tf.reduce_sum(tf.mul(labels * tf.log(softmax),
+                                           head), reduction_indices=[1])
+        else:
+            cross_entropy = -tf.reduce_sum(labels * tf.log(softmax), reduction_indices=[1]))
+
+        cross_entropy_mean = tf.reduce_mean(cross_entropy,
+                                            name='xentropy_mean')
+        tf.add_to_collection('losses', cross_entropy_mean)
+
+        loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
+    return loss
